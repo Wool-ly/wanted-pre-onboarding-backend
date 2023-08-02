c087ec401050a3b66d94780b26f267acc8249305
Woolly <wool_ly@naver.com>
2023-08-02 14:00:55 +0900

    Feat : 로그인 기능 구현, JWT 토큰, SpringSecurity 추가 - 로그인 기능 구현 - 로그인 Controller, Service, Repository 작성 - JWT 토큰, SpringSecurity 추가
    Related to: #3

--- src/main/java/com/preonboarding/wanted/config/SecurityConfig.java
+++ src/main/java/com/preonboarding/wanted/config/SecurityConfig.java
@@ -1,30 +1,65 @@
 package com.preonboarding.wanted.config;
 
+import com.preonboarding.wanted.exception.CustomAccessDeniedHandler;
+import com.preonboarding.wanted.exception.CustomAuthenticationEntryPoint;
+import com.preonboarding.wanted.security.JwtAuthenticationFilter;
+import com.preonboarding.wanted.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().disable()			//cors 방지
-                .csrf().disable()			//csrf 방지
-                .formLogin().disable()		//기본 로그인페이지 없애기
-                .headers().frameOptions().disable();
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
 
-        return http.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic().disable();
+
+        //csrf 사용안함
+        http.csrf().disable();
+
+        //세선사용 x
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //URL 인증여부 설정.
+        http.authorizeRequests()
+                .antMatchers( "/api/v1/users/signUp", "/", "/api/v1/users/login", "/css/**", "/exception/**", "/favicon.ico").permitAll()
+                .anyRequest().authenticated();
+
+        //JwtFilter 추가
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        //JwtAuthentication exception handling
+        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+        //access Denial handler
+        http.exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler());
+
     }
 
 }
--- src/main/java/com/preonboarding/wanted/entity/User.java
+++ src/main/java/com/preonboarding/wanted/entity/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.validation.constraints.Email;
 
@@ -19,6 +22,7 @@ import javax.validation.constraints.Email;
 @Getter
 @Entity
 @Table(name = "user")
+@NoArgsConstructor
 public class User extends BaseEntity implements Serializable {
 
     @Id
@@ -39,4 +43,23 @@ public class User extends BaseEntity implements Serializable {
     @OneToMany(mappedBy = "user")
     private List<Board> boardList = new ArrayList<>();
 
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING) //저장될때는 string으로 저장되도록
+    private UserRole role;
+
+    public User(String email, String password, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
