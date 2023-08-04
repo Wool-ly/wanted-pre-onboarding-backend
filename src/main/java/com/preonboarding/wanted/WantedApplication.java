9bca28a5998ed3795bcc5708035289fcbaae924a
Woolly <wool_ly@naver.com>
2023-08-05 01:08:23 +0900

    Feat : JPA Auditing 수정 - JPA Auditing 수정
    Related to: #4

--- src/main/java/com/preonboarding/wanted/WantedApplication.java
+++ src/main/java/com/preonboarding/wanted/WantedApplication.java
@@ -1,12 +1,19 @@
 package com.preonboarding.wanted;
 
+import com.preonboarding.wanted.utils.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class WantedApplication {
+
+	@Bean
+	public AuditorAwareImpl auditorAware() {
+		return new AuditorAwareImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WantedApplication.class, args);
--- src/main/java/com/preonboarding/wanted/entity/BaseEntity.java
+++ src/main/java/com/preonboarding/wanted/entity/BaseEntity.java
@@ -7,7 +7,9 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -26,5 +28,12 @@ public class BaseEntity {
     @Column(name = "updated_dt")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedDt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
 
 }
